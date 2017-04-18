8474b740e9eb5c4af9ddbe52f100de1a83a7e060
leoliao2008@163.com <leoliao2008@163.com>
2017-04-18 18:07:10 +0800

    minor change

--- app/src/main/java/com/liansong/blueled/activities/MainActivity.java
+++ app/src/main/java/com/liansong/blueled/activities/MainActivity.java
@@ -2,11 +2,28 @@ package com.liansong.blueled.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.liansong.blueled.R;
 import com.liansong.blueled.bases.BaseActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends BaseActivity {
+    private ListView lstv_record;
+    private TextView tv_timing;
+    private ArrayAdapter<String> mAdapter;
+    private ArrayList<String> arr_records =new ArrayList<>();
 
     public static void startActivity(Activity context){
         context.startActivity(new Intent(context,MainActivity.class));
@@ -20,16 +37,67 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        lstv_record= (ListView) findViewById(R.id.main_lstv_control_record);
+        tv_timing= (TextView) findViewById(R.id.main_tv_timing);
 
     }
 
     @Override
     protected void initData() {
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            try {
+                actionBar.setSubtitle("Ver "+getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_CONFIGURATIONS).versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        mAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, arr_records){
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                TextView view= (TextView) super.getView(position, convertView, parent);
+                view.setGravity(Gravity.CENTER);
+                return view;
+            }
+        };
+        lstv_record.setAdapter(mAdapter);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            case R.id.main_menu_scan_dev:
+                scanLeDevices();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     @Override
     protected void initListener() {
 
     }
+
+
+
+
+
+
 }
